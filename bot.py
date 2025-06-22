2de90293db52adeb7541e09829f1b5225f30d05a
Hugo Lopes <54045857+hugomvlopes@users.noreply.github.com>
2025-06-22 23:16:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-FOGOS_API = 'https://api.fogos.pt/v2/incidents/active?all=1&subRegion=Viseu%20D%C3%A3o%20Laf%C3%B5es'
+FOGOS_API = 'https://api.fogos.pt/v2/incidents/active?all=1'
 INTERVALO = 120  # segundos
 
 ocorrencias_enviadas = set()
@@ -13,24 +13,23 @@ def obter_ocorrencias():
     try:
         resposta = requests.get(FOGOS_API)
         if resposta.status_code == 200:
-            dados = resposta.json()
-            return [o for o in dados if o['concelho'] == "Oliveira de Frades"]
+            return resposta.json()
     except Exception as e:
         print(f"Erro: {e}")
     return []
 
 def enviar_alerta(ocorrencia):
-    mensagem = f"🚨 *Nova Ocorrência em Oliveira de Frades*\n\n" \
-               f"🕒 *Data:* {ocorrencia['date']} às {ocorrencia['hour']}\n" \
+    mensagem = f"🕒 *Data:* {ocorrencia['date']} às {ocorrencia['hour']}\n" \
                f"🚨 *Tipo:* {ocorrencia['natureza']}\n" \
                f"📍 *Local:* {ocorrencia['concelho']} / {ocorrencia['localidade']}\n" \
                f"📡 _Dados: Prociv / fogos.pt_"
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {'chat_id': CHAT_ID, 'text': mensagem, 'parse_mode': 'Markdown'}
-    requests.post(url, data=payload)
+    response = requests.post(url, data=payload)
+    print(f"Enviado: {mensagem}\nResposta: {response.status_code}")
 
-print("🚒 Bot de Alerta BVOFRADES iniciado...")
+print("🚒 Bot de Alerta BVOFRADES [MODO TESTE] iniciado...")
 while True:
     ocorrencias = obter_ocorrencias()
     for o in ocorrencias:
